27b3f3cee88963bed37e6c0fa74f8c1e13c2db47
jaquelinecatavares-cpu <jaqueline.c.a.tavares@gmail.com>
2025-12-15 11:01:36 -0100

    Add option menu to Streamlit app

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
+#import numpy as np
+#import pandas as pd
+#import matplotlib.pyplot
+
 st.header( "introduzindo os elementos do streamlit")
 menu = option_menu (menu_title="Menu",
                     options=["Início", "Gráficos Estatísticos", "Gráficos Dinâmicos", "Widgest", "Formulário"],
