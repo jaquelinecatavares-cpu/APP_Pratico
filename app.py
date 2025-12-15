099870e56558ad8af19ca9f42e49fb69e232fcbd
jaquelinecatavares-cpu <jaqueline.c.a.tavares@gmail.com>
2025-12-15 10:54:46 -0100

    Add menu options to Streamlit app

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
 import streamlit as st
 st.header( "introduzindo os elementos do streamlit")
+menu = option_menu (menu_title="Menu",
+                    options=[Início", "Gráficos Estatísticos", "Gráficos Dinâmicos", "Widgest", "Formulário"],
+                    icons=["house", "bar-chart", "bar-chart-line", "tooggles", "bar-chart"],
+                    menu_icon="cast",
+                    default_index=0,
+                    orientation="horizontal"
+                    
