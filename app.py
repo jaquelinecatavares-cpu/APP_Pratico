6d33556a4f0c7041659e32c2bfba1eb3ac9cf1ca
jaquelinecatavares-cpu <jaqueline.c.a.tavares@gmail.com>
2025-12-15 10:56:11 -0100

    Fix typo in Streamlit menu options

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.header( "introduzindo os elementos do streamlit")
 menu = option_menu (menu_title="Menu",
-                    options=[Início", "Gráficos Estatísticos", "Gráficos Dinâmicos", "Widgest", "Formulário"],
+                    options=["Início", "Gráficos Estatísticos", "Gráficos Dinâmicos", "Widgest", "Formulário"],
                     icons=["house", "bar-chart", "bar-chart-line", "tooggles", "bar-chart"],
                     menu_icon="cast",
                     default_index=0,
