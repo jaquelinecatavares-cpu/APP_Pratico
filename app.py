0ce613c9812b35cceeb4ee9a826050a2ecf7f590
jaquelinecatavares-cpu <jaqueline.c.a.tavares@gmail.com>
2025-12-15 12:01:31 -0100

    Uncomment pandas import in app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 #import numpy as np
-#import pandas as pd
+import pandas as pd
 #import matplotlib.pyplot
 
 st.header( "introduzindo os elementos do streamlit")
